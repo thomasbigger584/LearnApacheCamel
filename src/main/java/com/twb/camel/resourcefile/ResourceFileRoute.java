9c7493ae8eb7888bb07fd83acaa88066cd82d04f
Thomas Bigger <thomas@performanceactive.com>
2019-11-14 22:50:20 +0000

    using formatter toggle comments and tidyup

--- src/main/java/com/twb/camel/resourcefile/ResourceFileRoute.java
+++ src/main/java/com/twb/camel/resourcefile/ResourceFileRoute.java
@@ -11,21 +11,17 @@ import java.io.File;
 public class ResourceFileRoute extends SpringRouteBuilder {
     @Override
     public void configure() {
+        // @formatter:off
 
-//         route gets activated via direct.
-//         i.e. ProducerTemplate or some other route direct
         from("direct:resourceFile").
 
-//                Using a processor to get the file and
-//                place it into the message
                 process(new ResourceFileProcessor()).
 
-//                Verifying that the file is actually in the message
                 process(exchange -> {
                     Message message = exchange.getMessage();
                     File file = message.getBody(File.class);
-    String extension = FilenameUtils.getExtension(file.getName());
-    message.setHeader("FileExtension", extension);
+                    String extension = FilenameUtils.getExtension(file.getName());
+                    message.setHeader("FileExtension", extension);
                 }).
 
                 process(exchange -> {
@@ -33,12 +29,14 @@ public class ResourceFileRoute extends SpringRouteBuilder {
                 }).
 
                 choice().
-                when(header("FileExtension").isEqualTo("csv")).
-                to("jms:queue:csv.q").
-                when(header("FileExtension").isEqualTo("json")).
-                to("jms:queue:json.q").
-                when(header("FileExtension").isEqualTo("xml")).
-                to("jms:queue:xml.q").
+                    when(header("FileExtension").isEqualTo("csv")).
+                        to("jms:queue:csv.q").
+                    when(header("FileExtension").isEqualTo("json")).
+                        to("jms:queue:json.q").
+                    when(header("FileExtension").isEqualTo("xml")).
+                        to("jms:queue:xml.q").
                 endChoice();
+
+        // @formatter:on
     }
 }
